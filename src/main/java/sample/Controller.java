bbb93f59193dd8e8a26e33bdf94dccbe26cb3615
kanovas <kano.vas.2.0@gmail.com>
2017-05-14 03:50:51 +0300

    add power averages: running & overall

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -8,14 +8,14 @@ import javafx.scene.chart.XYChart;
 import org.jtransforms.fft.DoubleFFT_1D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.ResourceBundle;
 import java.util.Scanner;
-import java.util.function.DoubleConsumer;
+import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
 
@@ -38,10 +38,25 @@ public class Controller implements Initializable {
 
     private ArrayList<ArrayList<Double>> values = new ArrayList<>();
     private ArrayList<ArrayList<Double>> average = new ArrayList<>();
-    private ArrayList<Node> matrix = new ArrayList<>();
-    private int window = 10;
-    private int begin = 2000;
-    private int end = 2200;
+    private ArrayList<ArrayList<Double>> matrixPow = new ArrayList<>();    // alpha, beta, gamma, delta, theta
+    private ArrayList<ArrayList<Double>> matrixPowAvg = new ArrayList<>(); // *
+    private double[] rhythmAvg = new double[5];                            // *
+    private ArrayList<Number> symbols = new ArrayList<>();
+    private double[] spectrum;
+    private int powerWindow = 500;
+    private int begin = 1500;
+    private int end = 2500;
+
+    private final double DELTA_LOWER_BOUND = 1;
+    private final double DELTA_UPPER_BOUND = 4;
+    private final double THETA_LOWER_BOUND = 4;
+    private final double THETA_UPPER_BOUND = 8;
+    private final double ALPHA_LOWER_BOUND = 8;
+    private final double ALPHA_UPPER_BOUND = 13;
+    private final double BETA_LOWER_BOUND = 13;
+    private final double BETA_UPPER_BOUND = 30;
+    private final double GAMMA_LOWER_BOUND = 30;
+    private final double GAMMA_UPPER_BOUND = 120;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -49,17 +64,64 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         initValues();
-        countAverage();
+        average.addAll(values.stream().map(value1 -> countAverage(value1, 10)).collect(Collectors.toList()));
         n_channels = 2;
 
-        int diff = end - begin;
-        for (int i = begin; i < end; i++) {
-            PowerCounter powerCounter = new PowerCounter();
-            Arrays.stream(getSpectrum(values.get(0).subList(i - diff, i).stream().mapToDouble(j -> j).toArray())).forEach(powerCounter);
-            matrix.add(new Node(powerCounter.getMeanAlphaPower(), powerCounter.getMeanBetaPower(),powerCounter.getMeanGammaPower(),
-                    powerCounter.getMeanDeltaPower(), powerCounter.getMeanThetaPower(), values.get(0).get(i) < average.get(0).get(i) ? 0 : 1));
-            logger.info("i = " + i + "; alphaPower = " + powerCounter.getMeanAlphaPower());
+        for (int i = 0; i < 5; i++) {
+            matrixPow.add(new ArrayList<>());
         }
+        for (int i = begin; i < end; i++) {
+            //PowerCounter powerCounter = new PowerCounter();
+
+            double deltaPower = 0;
+            double thetaPower = 0;
+            double alphaPower = 0;
+            double betaPower = 0;
+            double gammaPower = 0;
+
+            //count powers
+            spectrum = getSpectrum(values.get(0).subList(i - powerWindow, i).stream().mapToDouble(j -> j).toArray());
+            for (int j = 0; j < spectrum.length; j++) {
+                double value = spectrum[j];
+                if (j >= DELTA_LOWER_BOUND && j < DELTA_UPPER_BOUND) {
+                    //deltaPower += Math.pow(10., value/10.);
+                    deltaPower += value;
+                } else if (j >= THETA_LOWER_BOUND && j < THETA_UPPER_BOUND) {
+                    //thetaPower += Math.pow(10., value/10.);
+                    thetaPower += value;
+                } else if (j >= ALPHA_LOWER_BOUND && j < ALPHA_UPPER_BOUND) {
+                    //alphaPower += Math.pow(10., value/10.);
+                    alphaPower += value;
+                } else if (j >= BETA_LOWER_BOUND && j < BETA_UPPER_BOUND) {
+                    //betaPower += Math.pow(10., value/10.);
+                    betaPower += value;
+                } else if (j >= GAMMA_LOWER_BOUND && j < GAMMA_UPPER_BOUND) {
+                    //gammaPower += Math.pow(10., value/10.);
+                    gammaPower += value;
+                }
+            }
+            //Arrays.stream(spectrum).forEach(powerCounter);
+            matrixPow.get(0).add(alphaPower / (ALPHA_UPPER_BOUND - ALPHA_LOWER_BOUND));
+            matrixPow.get(1).add(betaPower / (BETA_UPPER_BOUND - BETA_LOWER_BOUND));
+            matrixPow.get(2).add(gammaPower / (GAMMA_UPPER_BOUND - GAMMA_LOWER_BOUND));
+            matrixPow.get(3).add(deltaPower / (DELTA_UPPER_BOUND - DELTA_LOWER_BOUND));
+            matrixPow.get(4).add(thetaPower / (THETA_UPPER_BOUND - THETA_LOWER_BOUND));
+
+            symbols.add(values.get(0).get(i) < average.get(0).get(i) ? 0 : 1);
+        }
+        //count power averages
+        matrixPowAvg.addAll(matrixPow.stream().map(rhythm -> countAverage(rhythm, 100)).collect(Collectors.toList()));
+
+        //count overall average value for rhythm
+        for (int i = 0; i < 5; i++) {
+            rhythmAvg[i] = matrixPow.get(i).stream().mapToDouble(Double::doubleValue).sum() / (end - begin);
+        }
+
+        logger.info("Alpha average is " + rhythmAvg[0]);
+        logger.info("Beta average is " + rhythmAvg[1]);
+        logger.info("Gamma average is " + rhythmAvg[2]);
+        logger.info("Delta average is " + rhythmAvg[3]);
+        logger.info("Theta average is " + rhythmAvg[4]);
 
         initChart();
     }
@@ -70,10 +132,10 @@ public class Controller implements Initializable {
         //System.arraycopy(input, 0, fft, 0, input.length);
         //fft_1D.realForwardFull(fft);
         fft_1D.realForward(input);
-        double[] spectrum = new double[input.length/2];
+        double[] spectrum = new double[input.length / 2];
         spectrum[0] = input[0];
-        for (int k = 1; k < input.length/2; k++) {
-            spectrum[k] = Math.sqrt(input[2*k] * input[2*k] + input[2*k+1] * input[2*k+1]);
+        for (int k = 1; k < input.length / 2; k++) {
+            spectrum[k] = Math.sqrt(input[2 * k] * input[2 * k] + input[2 * k + 1] * input[2 * k + 1]);
         }
         return spectrum;
     }
@@ -108,22 +170,21 @@ public class Controller implements Initializable {
         logger.info("channels " + n_channels);
     }
 
-    private void countAverage() {
+    private ArrayList<Double> countAverage(ArrayList<Double> values, int window) {
 
-        for (ArrayList<Double> channel : values) {
-            ArrayList<Double> channelAvg = new ArrayList<>();
-            double currentSum = 0;
-            for (int j = 0; j < channel.size(); j++) {
-                if (j < window) {
-                    currentSum += channel.get(j);
-                    channelAvg.add(0.);
-                } else {
-                    currentSum += channel.get(j) - channel.get(j - window);
-                    channelAvg.add(currentSum / window);
-                }
+        ArrayList<Double> res = new ArrayList<>();
+        double currentSum = 0;
+        for (int j = 0; j < values.size(); j++) {
+            if (j < window) {
+                currentSum += values.get(j);
+                res.add(currentSum / (j + 1));
+            } else {
+                currentSum += values.get(j) - values.get(j - window);
+                res.add(currentSum / window);
             }
-            average.add(channelAvg);
         }
+        return res;
+
     }
 
     private void initChart() {
@@ -152,85 +213,15 @@ public class Controller implements Initializable {
         lineChart1.getData().add(seriesAvg);
 
         lineChart2.setAnimated(false);
-        /*i = 1;
         XYChart.Series series2 = new LineChart.Series<Double, Double>();
         XYChart.Series series2Avg = new LineChart.Series<Double, Double>();
-        series2.setName((i + 1) + " channel");
-        series2Avg.setName((i + 1) + " channel average");
-        for (int j = begin; j < end && j < values.get(i).size(); j++) {
-            series2.getData().add(new XYChart.Data(j, values.get(i).get(j)));
-            series2Avg.getData().add(new XYChart.Data(j, average.get(i).get(j)));
+        series2.setName("Alpha rhythm power graph for channel " + (i + 1));
+        series2Avg.setName("Alpha rhythm average for channel " + (i + 1));
+        for (int j = 0; j < matrixPow.get(i).size(); j++) {
+            series2.getData().add(new XYChart.Data(begin + j, matrixPow.get(i).get(j)));
+            series2Avg.getData().add(new XYChart.Data(begin + j, matrixPowAvg.get(i).get(j)));
         }
         lineChart2.getData().add(series2);
         lineChart2.getData().add(series2Avg);
-*/
-        XYChart.Series series2 = new LineChart.Series<Double, Double>();
-        series2.setName("Alpha power graph for channel " + (i + 1));
-        for (int j = 0; j < matrix.size(); j++) {
-            series2.getData().add(new XYChart.Data(begin + j, matrix.get(j).alphaPower));
-        }
-        lineChart2.getData().add(series2);
-    }
-    private class PowerCounter implements DoubleConsumer {
-        private final double DELTA_LOWER_BOUND = 1;
-        private final double DELTA_UPPER_BOUND = 4;
-        private final double THETA_LOWER_BOUND = 4;
-        private final double THETA_UPPER_BOUND = 8;
-        private final double ALPHA_LOWER_BOUND = 8;
-        private final double ALPHA_UPPER_BOUND = 13;
-        private final double BETA_LOWER_BOUND = 13;
-        private final double BETA_UPPER_BOUND = 30;
-        private final double GAMMA_LOWER_BOUND = 30;
-        private final double GAMMA_UPPER_BOUND = 80;
-
-        int deltaNum = 0; int thetaNum = 0; int alphaNum = 0; int betaNum = 0; int gammaNum = 0;
-        double deltaPower = 0; double thetaPower = 0; double alphaPower = 0; double betaPower = 0; double gammaPower = 0;
-
-        @Override
-        public void accept(double value) {
-            if (value >= DELTA_LOWER_BOUND && value < DELTA_UPPER_BOUND) {
-                deltaNum++;
-                deltaPower += Math.pow(10., value/10.);
-
-            }
-            else if (value >= THETA_LOWER_BOUND && value < THETA_UPPER_BOUND) {
-                thetaNum++;
-                thetaPower += Math.pow(10., value/10.);
-            }
-            else if (value >= ALPHA_LOWER_BOUND && value < ALPHA_UPPER_BOUND) {
-                alphaNum++;
-                alphaPower += Math.pow(10., value/10.);
-            }
-            else if (value >= BETA_LOWER_BOUND && value < BETA_UPPER_BOUND) {
-                betaNum++;
-                betaPower += Math.pow(10., value/10.);
-            }
-            else if (value >= GAMMA_LOWER_BOUND && value < GAMMA_UPPER_BOUND) {
-                gammaNum++;
-                gammaPower += Math.pow(10., value/10.);
-            }
-        }
-        public double getMeanAlphaPower() { return alphaNum == 0 ? 0 : alphaPower / alphaNum;}
-        public double getMeanBetaPower() {return betaPower == 0 ? 0 : betaPower / betaNum;}
-        public double getMeanGammaPower() {return gammaPower == 0 ? 0 : gammaPower / gammaNum;}
-        public double getMeanDeltaPower() {return deltaPower == 0 ? 0 : deltaPower / deltaNum;}
-        public double getMeanThetaPower() {return thetaPower == 0 ? 0 : thetaPower / thetaNum;}
-    }
-    private class Node {
-        double alphaPower;
-        double betaPower;
-        double gammaPower;
-        double deltaPower;
-        double thetaPower;
-        int symbol;
-
-        Node(double alphaPower, double betaPower, double gammaPower, double deltaPower, double thetaPower, int symbol) {
-            this.alphaPower = alphaPower;
-            this.betaPower = betaPower;
-            this.deltaPower = deltaPower;
-            this.gammaPower = gammaPower;
-            this.thetaPower = thetaPower;
-            this.symbol = symbol;
-        }
     }
 }
